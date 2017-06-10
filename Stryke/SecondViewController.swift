d357721bae534ef812bdaafd4f6bb5799774aa94
Michael Auer <mauermcx@gmail.com>
2017-06-09 23:57:04 -0400

    Initial Commit

---
+++ Stryke/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Stryke
+//
+//  Created by Michael Andrew Auer on 6/9/17.
+//  Copyright © 2017 Pear Bus. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
