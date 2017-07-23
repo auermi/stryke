4e00b7685ddf29000150f323990b37230ad19e5e
Michael Auer <mauermcx@gmail.com>
2017-07-22 21:01:50 -0400

    Change name of second view controller to news view controller

---
+++ Stryke/NewsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NewsViewController.swift
+//  Stryke
+//
+//  Created by Michael Andrew Auer on 6/9/17.
+//  Copyright © 2017 Pear Bus. All rights reserved.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Look at the news!")
+        
+    }
+
+}
+
